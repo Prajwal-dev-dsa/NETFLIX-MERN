22159cffaaed9d4e89904d5035020c45ddba9534
Prajwal <prajwal77dwivedi@gmail.com>
2025-07-07 00:34:56 +0530

    Protected Route Added

---
+++ backend/middleware/protectedRoute.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { ENV_VARS } from "../config/envVars.js";
+import User from "../models/user.model.js";
+
+export const protectedRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies["jwt-netflix"]; //this is the name of the cookie that we set in the frontend
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized - No token provided" });
+    }
+    const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET); //this is the secret key that we set in the env file and we use it here to verify the token
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized - Invalid token" });
+    }
+    const user = await User.findById(decoded.userId).select("-password"); //this is the user that we found in the database and we select the password field to not send it to the frontend
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized - User not found" });
+    }
+    req.user = user; //this is the user that we found in the database and we send it to the next middleware
+    next();
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 import express from "express";
+
 import authRoutes from "./routes/auth.route.js";
+import { protectedRoute } from "./middleware/protectedRoute.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); //this is used to parse the body of the request
+app.use(cookieParser()); //this is used to parse the cookies of the request
 
 app.use("/api/v1/auth", authRoutes); //authentication route
-app.use("/api/v1/movie", movieRoutes); //movie route
-app.use("/api/v1/tv", tvRoutes); //tv route
+app.use("/api/v1/movie", protectedRoute, movieRoutes); //movie route
+app.use("/api/v1/tv", protectedRoute, tvRoutes); //tv route
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
